a03434a87d1694ee806088d0edcfce235a3bb39a
Joana Mateos <joanamateos@hotmail.com>
2022-07-19 11:44:11 +0000

    Ejercicio terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,179 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export function Home() {
+	const [todos, setTodos] = useState([]);
 
-//create your first component
-const Home = () => {
+	useEffect(() => {
+		getAPI();
+	}, []);
+
+	const getAPI = () => {
+		let todoLink =
+			"https://assets.breatheco.de/apis/fake/todos/user/joanamateos";
+
+		const fetchTodo = async () => {
+			let todosList = await fetch(todoLink, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => {
+					if (resp.status == 404) {
+						createAPI();
+					} else {
+						return resp.json();
+					}
+				})
+				.then(data => setTodos(data))
+				.catch(error => console.log(error));
+		};
+		fetchTodo();
+	};
+
+	const createAPI = () => {
+		let todoLink =
+			"https://assets.breatheco.de/apis/fake/todos/user/joanamateos";
+
+		const fetchTodo = async () => {
+			let todosList = await fetch(todoLink, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify([])
+			})
+				.then(resp => {
+					if (resp.status == 200) {
+						getAPI();
+					}
+				})
+				.then(data => console.log(data))
+				.catch(error => console.log(error));
+		};
+		fetchTodo();
+	};
+
+	const updateAPI = newData => {
+		let todoLink =
+			"https://assets.breatheco.de/apis/fake/todos/user/joanamateos";
+
+		const fetchTodo = async () => {
+			let todosList = await fetch(todoLink, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(newData)
+			})
+				.then(resp => {
+					if (resp.status == 200) {
+						setTodos(newData);
+					}
+				})
+				.then(data => console.log(data))
+				.catch(error => console.log(error));
+		};
+		fetchTodo();
+	};
+
+	const deleteAPI = () => {
+		let todoLink =
+			"https://assets.breatheco.de/apis/fake/todos/user/joanamateos";
+
+		const fetchTodo = async () => {
+			let todosList = await fetch(todoLink, {
+				method: "DELETE"
+			})
+				.then(resp => {
+					if (resp.status == 200) {
+						setTodos([]);
+						return resp;
+					}
+				})
+				.then(data => console.log(data))
+				.catch(error => console.log(error));
+		};
+		fetchTodo();
+	};
+
+	const [inputValue, setInputValue] = useState("");
+
+	const addTodo = e => {
+		if (e.key == "Enter") {
+			if (inputValue !== "") {
+				const todosNew = todos.concat({
+					label: inputValue,
+					done: false
+				});
+				updateAPI(todosNew);
+				setInputValue("");
+			} else alert("Introduce una tarea");
+		}
+	};
+	const removeTodo = task => {
+		const removeItem = todos.filter(item => item.label !== task);
+		if (removeItem.length > 0) {
+			updateAPI(removeItem);
+		} else {
+			deleteAPI();
+		}
+	};
+	const Todolist = () => {
+		if (todos.length > 0) {
+			return (
+				<div>
+					<ul className="list-group list-group-flush">
+						{todos.map((item, index) => (
+							<li
+								className="list-group-item d-flex justify-content-between align-items-center"
+								key={index}>
+								{item.label}
+								<button
+									onClick={() => removeTodo(item.label)}
+									className="btn btn-link">
+									<i className="fas fa-times"></i>
+								</button>
+							</li>
+						))}
+					</ul>
+					<hr className="solid" />
+					<div className="float-left text-muted mt-2">
+						{todos.length} tareas pendientes por completar
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<p>No hay tareas registradas, añade una tarea</p>
+				</div>
+			);
+		}
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center mt-5 justify-content-center">
+			<h1>Lista de tareas de Joana Mateos</h1>
+			<div className="card cardtodos mx-auto">
+				<div className="card-body">
+					<div className="input-group mb-3 input-group-lg">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="¿Qué tareas tienes pendientes?"
+							value={inputValue}
+							onChange={e => setInputValue(e.target.value)}
+							onKeyUp={addTodo}
+						/>
+					</div>
+					<Todolist />
+				</div>
+				<div className="card-footer">
+					<button type="button" onClick={() => deleteAPI()}>
+						Borrar la lista
+					</button>
+				</div>
+			</div>
 		</div>
 	);
-};
-
-export default Home;
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
+
+
+//include your index.scss file into the bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Home } from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
